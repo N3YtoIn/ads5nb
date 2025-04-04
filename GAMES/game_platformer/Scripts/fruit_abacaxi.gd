44da3d8b120286597c418a30b5a71be4fb8b0225
N3YtoIn <nathan.dev2004@gmail.com>
2025-04-04 19:17:07 -0300

    feat: aulas: 04.04.2025

---
+++ GAMES/game_platformer/Scripts/fruit_abacaxi.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+func _on_area_entered(area: Area2D) -> void:
+	
+	if area.is_in_group("Player"):
+		$AnimatedSprite2D.play("collected")
+		$CollisionShape2D.queue_free()
+		
+		await get_tree().create_timer(1).timeout
+		queue_free()
+	
+	pass
